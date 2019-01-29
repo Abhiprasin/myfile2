ce980f94c1d607733134cca7fb8d2ff59a368cca
Abhishek Pratap Singh <abhishek.pratap@vvdntech.in>
2019-01-29 10:15:33 +0530

    Update for Multiplication

--- calculator.c
+++ calculator.c
@@ -1,16 +1,18 @@
 #include<stdio.h>
 int addition(int, int);
 int subtraction(int, int);
+int multiplication(int, int);
 void display(int);
 int main(){
 	int a=0,b=0,c=0;
-	printf("Enter your choice:\n1.Addition\n2.Subtraction\n");
+	printf("Enter your choice:\n1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Modulo\n");
 	scanf("%d",&a);
 	printf("Enter two values\n");
 	scanf("%d,%d",&b,&c);
 	switch(a){
 	case 1: display(addition(b,c));break;
 	case 2: display(subtraction(b,c));break;
+	case 3: display(multiplication(b,c));break;
 	default : printf("Invalid");
 	}
 	return 0;	
@@ -20,6 +22,9 @@ return x+y;
 }
 int subtraction(int x, int y){
 return x-y;
+}
+int multiplication(int x, int y){
+return x*y;
 }
 void display(int a){
 printf("the result is %d\n", a);
