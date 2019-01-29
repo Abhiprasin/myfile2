76643e923c7da5cd0b884be5fb27053e8e2f6690
Abhishek Pratap Singh <abhishek.pratap@vvdntech.in>
2019-01-29 10:01:49 +0530

    Update Feature Subtraction

--- calculator.c
+++ calculator.c
@@ -1,16 +1,25 @@
 #include<stdio.h>
 int addition(int, int);
+int subtraction(int, int);
 void display(int);
 int main(){
-	int b=0,c=0;
+	int a=0,b=0,c=0;
+	printf("Enter your choice:\n1.Addition\n2.Subtraction\n");
+	scanf("%d",&a);
 	printf("Enter two values\n");
 	scanf("%d,%d",&b,&c);
-	display(addition(b,c));
+	switch(a){
+	case 1: display(addition(b,c));break;
+	case 2: display(subtraction(b,c));break;
+	default : printf("Invalid");
+	}
 	return 0;	
 	}
-
 int addition(int x, int y){
 return x+y;
+}
+int subtraction(int x, int y){
+return x-y;
 }
 void display(int a){
 printf("the result is %d\n", a);
